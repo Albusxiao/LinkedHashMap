fe93b672f835ad5f93f30458dce72cc6bffb01b1
Albus <albus.xiao@sjtu.edu.cn>
2026-03-23 23:29:17 +0800

    feat:hash_map finished

--- lru/lru.hpp
+++ lru/lru.hpp
@@ -9,7 +9,7 @@
 #include "utility.hpp"
 
 using std::vector;
-
+using std::list;
 class Hash {
 public:
     unsigned int operator()(Integer lhs) const {
@@ -225,13 +225,20 @@ namespace sjtu {
 
     template<class Key, class T, class Hash = std::hash<Key>, class Equal = std::equal_to<Key> >
     class hashmap {
+        const double load_factor=0.75;
     public:
         using value_type = pair<const Key, T>;
+        using bucket_type = list<value_type>;
+        using hash_type=vector<bucket_type>;
         /**
          * elements
          * add whatever you want
          */
-        vector<double_list<value_type>> hash_map;
+        hash_type hash_map;
+        /**
+         * size: the length of hashmap
+         * current: the actual number of elements in hashmap
+        */
         size_t size,current;
         // --------------------------
 
@@ -239,7 +246,7 @@ namespace sjtu {
          * the follows are constructors and destructors
          * you can also add some if needed.
          */
-        hashmap():size(100),current(0) {hash_map.assign(100,double_list<value_type>());}
+        hashmap():size(131),current(0) {hash_map.assign(131,list<value_type>());}
         hashmap(const hashmap &other):size(other.size),hash_map(other.hash_map),current(other.current){}
 
         ~hashmap() {
@@ -249,67 +256,87 @@ namespace sjtu {
 
         hashmap &operator=(const hashmap &other) {
             size=other.size;
-            hash_map.assign(size,double_list<value_type>());
-            for (int i=0;i<size;i++) {hash_map[i] = other.hash_map[i];}
+            hash_map=other.hash_map;
+            current=other.current;
             return *this;
         }
 
         class iterator {
+
         public:
             /**
              * elements
              * add whatever you want
              */
-            int mp_index;
-            double_list<value_type>::iterator ls_it;
+            hash_type* bucket;
+            size_t bucket_index;
+            bucket_type::iterator bucket_iter;
             // --------------------------
             /**
              * the follows are constructors and destructors
              * you can also add some if needed.
              */
-            iterator():mp_index(-1),ls_it() {
+            iterator():bucket(nullptr),bucket_index(0),bucket_iter(){}
+            iterator(const iterator &t):bucket_index(t.bucket_index),bucket_iter(t.bucket){
+                bucket_iter=t.bucket_iter;
             }
 
-            iterator(const iterator &t) {
-            }
-
-            ~iterator() {
-            }
+            ~iterator() =default;
 
             /**
              * if point to nothing
              * throw
              */
             value_type &operator*() const {
+                if (bucket_iter==nullptr)throw "*nullptr";
+                return *bucket_iter;
             }
 
             /**
              * other operation
              */
             value_type *operator->() const noexcept {
+                if (bucket_iter==nullptr)throw "*nullptr";
+                return *bucket_iter;
             }
 
             bool operator==(const iterator &rhs) const {
+                if (bucket==nullptr&&rhs.bucket==nullptr)return true;
+                else if (bucket==rhs.bucket)return bucket_iter==rhs.bucket_iter;
+                return false;
             }
 
             bool operator!=(const iterator &rhs) const {
+                return !(*this== rhs);
             }
         };
 
         void clear() {
-
+            for (auto i:hash_map)i.clear();
+            current=0;
         }
 
         /**
          * you need to expand the hashmap dynamically
          */
         void expand() {
+            size<<=1;
+            hash_type tmp;
+            tmp.assign(size,bucket_type());
+            Hash h;
+            for (auto i:hash_map) {
+                for (auto j:i) {
+                    tmp[h(j.first)%size].emplace_back(j);
+                }
+            }
+            hash_map=move(tmp);
         }
 
         /**
          * the iterator point at nothing
          */
         iterator end() const {
+            return iterator(hash_map,hash_map.size()-1,hash_map.end());
         }
 
         /**
@@ -317,6 +344,11 @@ namespace sjtu {
          * not find, return the end (point to nothing)
          */
         iterator find(const Key &key) const {
+            Hash h;
+            size_t pos=h(key)%size;
+            for (auto it=hash_map[pos].begin();it!=hash_map[pos].end();++it)
+                if (it->firs==key)return iterator(hash_map,pos,it);
+            return end();
         }
 
         /**
@@ -326,6 +358,17 @@ namespace sjtu {
          * -> insert the value_pair, return true
          */
         sjtu::pair<iterator, bool> insert(const value_type &value_pair) {
+            iterator it=find(value_pair.first);
+            bool hash_bool=true;
+            if (it!=hash_map.end())it->second=value_pair.second,hash_bool=false;
+            else {
+                ++current;
+                if (size*load_factor<current)expand();
+                Hash h;
+                size_t pos=h(value_pair.first)%size;
+                it=(hash_map,pos,hash_map[pos].emplace_back(value_pair));
+            }
+            return pair<iterator,bool>{it,hash_bool};
         }
 
         /**
@@ -333,6 +376,12 @@ namespace sjtu {
          * otherwise, return false
          */
         bool remove(const Key &key) {
+            iterator it=find(key);
+            if (it!=hash_map.end()) {
+                hash_map[it.bucket_index].erase(it.bucket_iter);
+                return true;
+            }
+            else return false;
         }
     };
 
